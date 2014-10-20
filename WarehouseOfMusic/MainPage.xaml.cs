18b8cb05bf70cf074974d19e1b2216617c016c65
usehotkey <igolopolosov@gmail.com>
2014-10-20 22:11:03 +0400

    #6 Add ProjectEditorPage Added empty page for editing projects.

--- WarehouseOfMusic/MainPage.xaml.cs
+++ WarehouseOfMusic/MainPage.xaml.cs
@@ -10,30 +10,16 @@ namespace WarehouseOfMusic
         public MainPage()
         {
             InitializeComponent();
-
-            // Пример кода для локализации ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/ApplicationSettings.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/ApplicationSettingsPage.xaml", UriKind.Relative));
         }
 
-        // Пример кода для сборки локализованной панели ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Установка в качестве ApplicationBar страницы нового экземпляра ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Создание новой кнопки и установка текстового значения равным локализованной строке из AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Создание нового пункта меню с локализованной строкой из AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void CreateProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ProjectEditorPage.xaml", UriKind.Relative));
+        }
     }
 }
---
+++ WarehouseOfMusic/ProjectEditorPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Phone.Controls;
+namespace WarehouseOfMusic
+{
+    public partial class ProjectEditorPage : PhoneApplicationPage
+    {
+        public ProjectEditorPage()
+        {
+            InitializeComponent();
+            // Пример кода для локализации ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+        // Пример кода для сборки локализованной панели ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Установка в качестве ApplicationBar страницы нового экземпляра ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Создание новой кнопки и установка текстового значения равным локализованной строке из AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Создание нового пункта меню с локализованной строкой из AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
