2eeb3fc13e2dd838e3f272d5a1133c4e08483986
usehotkey <igolopolosov@gmail.com>
2014-11-12 21:00:36 +0300

    Add the opportunity to start to edit a project from the list #14 Page of editing open when user pressing on the project.

--- WarehouseOfMusic/ExistingProjectsPage.xaml.cs
+++ WarehouseOfMusic/ExistingProjectsPage.xaml.cs
@@ -6,7 +6,11 @@
 
 namespace WarehouseOfMusic
 {
+    using System;
+    using System.Windows.Controls;
+    using System.Windows.Input;
     using Microsoft.Phone.Controls;
+    using Model;
 
     /// <summary>
     /// Page with existing projects
@@ -20,6 +24,24 @@ namespace WarehouseOfMusic
         {
             this.InitializeComponent();
             this.DataContext = App.ViewModel;
+        }
+
+        /// <summary>
+        /// Chose project for editing.
+        /// </summary>
+        /// <param name="sender">Project item displayed like a list box item</param>
+        /// <param name="e">One tap</param>
+        private void ProjectItemGrid_OnTap(object sender, GestureEventArgs e)
+        {
+            var grid = sender as Grid;
+
+            if (grid != null)
+            {
+                var chosenProject = grid.DataContext as ToDoProject;
+                App.ViewModel.CurrentProject = chosenProject;
+            }
+
+            NavigationService.Navigate(new Uri("/ProjectEditorPage.xaml", UriKind.Relative));
         }
     }
 }
