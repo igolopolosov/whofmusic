3e976fe6ccd0e176df1b45798bed11795dcc7574
usehotkey <igolopolosov@gmail.com>
2014-11-20 18:25:23 +0300

    Make application bar for EditProject page #20 Added app bar with buttons: help and settings

--- WarehouseOfMusic/ProjectEditorPage.xaml.cs
+++ WarehouseOfMusic/ProjectEditorPage.xaml.cs
@@ -6,10 +6,13 @@
 
 namespace WarehouseOfMusic
 {
+    using System;
     using System.Windows;
     using System.Windows.Controls;
     using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
     using Model;
+    using Resources;
 
     /// <summary>
     /// Page of editing projects
@@ -23,8 +26,10 @@ namespace WarehouseOfMusic
         {
             this.InitializeComponent();
             this.DataContext = App.ViewModel;
+            this.BuildLocalizedAppBar();
         }
 
+        #region Track editing(add, delete)
         /// <summary>
         /// Adding new track
         /// </summary>
@@ -51,6 +56,42 @@ namespace WarehouseOfMusic
             }
 
             this.Focus();
+        } 
+        #endregion
+
+        #region For application bar
+        /// <summary>
+        /// Click on SettingsButton
+        /// </summary>
+        /// <param name="sender">Some object</param>
+        /// <param name="e">Click on button</param>
+        private void SettingsButton_OnClick(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ApplicationSettingsPage.xaml", UriKind.Relative));
         }
+
+        /// <summary>
+        /// Bulid Localized application bar
+        /// </summary>
+        private void BuildLocalizedAppBar()
+        {
+            this.ApplicationBar = new ApplicationBar();
+
+            //// Add button linked with help page
+            var helpButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.question.png", UriKind.Relative))
+            {
+                Text = AppResources.AppBarHelp
+            };
+            this.ApplicationBar.Buttons.Add(helpButton);
+
+            //// Add button linked with settings page
+            var settingsButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.settings.png", UriKind.Relative))
+            {
+                Text = AppResources.AppBarSettings,
+            };
+            settingsButton.Click += this.SettingsButton_OnClick;
+            this.ApplicationBar.Buttons.Add(settingsButton);
+        }
+        #endregion
     }
 }
